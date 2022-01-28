4a94203b4c6bc8b7d4d85bcd0663283437f6c4ce
Daarrik <darrikwh@gmail.com>
2022-01-28 00:55:46 -0800

    more prettier

--- app/app.js
+++ app/app.js
@@ -1,12 +1,12 @@
-const socket = io('ws://localhost:3000');
+const socket = io("ws://localhost:3000");
 
-socket.on('message', text => {
-  const el = document.createElement('li');
+socket.on("message", (text) => {
+  const el = document.createElement("li");
   el.innerHTML = text;
-  document.querySelector('ul').appendChild(el);
+  document.querySelector("ul").appendChild(el);
 });
 
-document.querySelector('button').onclick = () => {
-  const text = document.querySelector('input').value;
-  socket.emit('message', text);
+document.querySelector("button").onclick = () => {
+  const text = document.querySelector("input").value;
+  socket.emit("message", text);
 };
