2613c23d90cdfdb75d1d4eba6f62d52e40e4169a
Daarrik <darrikwh@gmail.com>
2022-01-28 00:48:37 -0800

    prettier formatting

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const socket = require('socket.io')
+const http = require("http");
+const server = http.Server(app);
+const socket = require("socket.io");
 const io = socket(server, {
-  cors: { origin: "*" }
+  cors: { origin: "*" },
 });
-const PORT = 3000
+const PORT = 3000;
 
-io.on('connection', socket => {
-  console.log('A user connected');
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
-  socket.on('message', message => {
+  socket.on("message", (message) => {
     console.log(message);
-    io.emit('message', `${socket.id.substr(0,2)}: ${message}`);
+    io.emit("message", `${socket.id.substr(0, 2)}: ${message}`);
   });
 });
 
